4318d89d8f0acc731162c6d16abb04b0ca7f872e
Askar <42945205+Feinei@users.noreply.github.com>
2020-04-07 10:58:00 +0300

    auth commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Article> Article { get; set; }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,11 @@
+namespace SocialNetwork.Models
+{
+    public class Article : Post
+    {
+        public string Title { get; set; }
+
+        public Article() : base() {
+            this.Title = "";
+        }
+    }
+}
